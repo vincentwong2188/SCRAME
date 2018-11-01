a9adc0b9745df67f81403220f44a7cd46de93922
Shen Chen <shenc.96@gmail.com>
2018-11-01 17:13:45 +0800

    added my own class for testing

--- src/Shenc.java
+++ src/Shenc.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Shenc {
     public static void main(String[] args){
         System.out.println("This is mine no touchie!");
+
+        ArrayList<Course> courseList = new ArrayList<>(0);
+
+        ArrayList<Student> studentList = new ArrayList<>(0);
+        //studentList.add();
     }
 }
