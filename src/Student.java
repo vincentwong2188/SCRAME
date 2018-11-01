61a9514c592c3d183ad4734d12c68919b7d4e24e
Zachary Chua <zchua025@e.ntu.edu.sg>
2018-11-01 18:23:15 +0800

    hi

--- src/Student.java
+++ src/Student.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 public class Student {
     private int matric;
     private ArrayList<String> courseCodes; //can i just use courseCode instead of an array of course obj
-    private Assessment[] listAssessments;
+    private ArrayList<Assessment> assessments;
 
     Scanner sc=new Scanner(System.in);
 
     public Student(){
+        courseCodes= new ArrayList<>();
+        assessments= new ArrayList<>();
     }
 
     public Student(int matric){
         this.matric=matric;
         courseCodes= new ArrayList<>();
+        assessments= new ArrayList<>();
     }
 
     public Student( int matric, ArrayList<String> courseCodes){
         this.matric=matric;
         this.courseCodes= courseCodes;
+        assessments= new ArrayList<>();
     }
 
     public int getMatric(){
@@ -39,22 +43,22 @@ public class Student {
         this.courseCodes.add(courseCode);
     }
 
-    public Assessment[] fillListAssessments(){ //get an array of assessments for this student
+    public ArrayList<Assessment> fillAssessments(){ //get an array of assessments for this student
         for(int i=0;i<courseCodes.size();i++) {
             String courseCode = courseCodes.get(i); //to be implemented
-            listAssessments[i]= new Assessment(courseCode);
+            assessments.add(new Assessment(courseCode));
             System.out.println("Please enter the student's Exam Marks:");
             double examMark = sc.nextDouble();
-            listAssessments[i].setExamMark(examMark);
+            assessments.get(i).setExamMark(examMark);
             System.out.println("Please enter the student's Course Marks:");
             double courseMark = sc.nextDouble();
-            listAssessments[i].setCourseMark(courseMark); //within this has the component alr
+            assessments.get(i).setCourseMark(courseMark); //within this has the component alr
         }
-        return listAssessments;
+        return assessments;
     }
 
     public void printTranscript(){
-        for(int i=0;i<listAssessments.length;i++)
-            listAssessments[i].printCourseTranscript(); //need to modify to print components
+        for(int i=0;i<assessments.size();i++)
+            assessments.get(i).printCourseTranscript(); //need to modify to print components
     }
 }
