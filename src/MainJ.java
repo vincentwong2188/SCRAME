c14478ebff50a5ddfe305927cee1b53bbe447b0b
janssentan <janssentan96@gmail.com>
2018-11-01 22:57:00 +0800

    dafsdf

--- src/MainJ.java
+++ src/MainJ.java
@@ -15,4 +15,32 @@
 
 
 public class MainJ {
+
+public String getLecturer(String classID) {                //lecture list is within course which is within courselist
+    for (int i = 0; i < lectureList.size(); i++) {
+        if (lectureList.get(i).getClassID().equals(classID)) {
+            return lectureList.get(i).getLecturer();
+        }
+    }
+}
+
+public String getTutor(String classID)  {
+    for (int i = 0; i < tutorialList.size(); i++)   {
+        if (tutorialList.get(i).getClassID().equals(classID))   {
+            return tutorialList.get(i).getTutorial;                 // returning the method gives the return of variable tutor
+        }
+    }
+}
+
+
+public int getClassSize(String classID){
+    for (int i = 0; i < tutorialList.size(); i++)   {
+        if (tutorialList.get(i).getClassID().equals(ClassID))   {
+            return getClassSize.get(i).getClassSize;
+        }
+    }
+}
+
+
+
 }
