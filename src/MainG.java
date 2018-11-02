dc9e151c85ba39b7d7f7d90623f3e5f5573f8dc2
gwynethang <gang007@e.ntu.edu.sg>
2018-11-01 17:19:05 -0700

    hiiii

--- src/MainG.java
+++ src/MainG.java
@@ -1,3 +1,40 @@
 public class MainG {
-    public static void main(String[] args) {System.out.println("G")}
+   //  public static void main(String[] args) {System.out.println("G")};
+    /* public String addCourse(String courseCode) { // should this whole thing should be in an App Class cos this is an abstract class
+        System.out.println("What is the new course code?");
+        String newCourseCode = sc.next();
+        System.out.println("How many slots are there in" + newCourseCode + "?");
+        int totalSlots = sc.nextInt();
+        System.out.println("What lessons will the new course have?");
+        System.out.println("1. 1 Lecture");
+        System.out.println("2. 1 Lecture and 2 Tutorials");
+        System.out.println("3. 1 Lecture, 2 Tutorials and 2 Labs");
+        int choice = sc.nextInt();
+        ArrayList<Course> courseList = new ArrayList<>(); //eg: List<Model> models = new ArrayList<>();
+        switch (choice) {
+            case 1:
+                String details = "1 Lecture";
+                courseList.add(new Course(String courseCode, details));
+                break;
+
+            case 2:
+                String details2 = "1 Lecture and 2 Tutorials"; //why cannot use details2
+                courseList.add(new Course(String courseCode, details2));
+                break;
+
+            case 3:
+                String details3 = "1 Lecture,  Tutorials and 2 Labs";
+                courseList.add(new Course(String courseCode, details3));
+                break;
+
+            default:
+
+                System.out.println("You have chosen an invalid option. Please enter the option again.");
+        }
+        for (int i = 0; i < courseList.size(); i++) {
+            System.out.println(courseList.get(i));
+        }
+        return "Your course has been successfully added!";
+    } */
+
 }
